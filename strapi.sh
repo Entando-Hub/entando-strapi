21dad7ed8ac8778d25a6f878baaa67e592449606
Kamlesh Bobde <kamlesh.bobde@newvisionsoftware.in>
2022-09-15 01:55:54 +0530

    Script changes, updated ingress path

--- strapi.sh
+++ strapi.sh
@@ -1,61 +1,69 @@
 #!/bin/bash
+echo "SCRIPT STARTED"
 
-echo "SCRIPT STARTED..."
 
-# Reading Token which is required to access Kubernates API (this token is already present when pod is up)
+# Reading Token which is required to access Kubernates APi (this token is already present when pod is up)
 KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 if [ ! -z "$KUBE_TOKEN" ]
 then
-  echo "Kubernetes TOKEN FETCHED"
+ echo "TOKEN FETCHED"
 fi
 
-# Reading ingress host and set it as ENV 
-export PUBLIC_BASE_URL=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN"  https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/networking.k8s.io/v1/namespaces/entando/ingresses/quickstart-ingress | jq .spec.rules[0].host | tr -d '"')
-echo "Public base url: "$PUBLIC_BASE_URL
-echo "Server servlet context path: "$SERVER_SERVLET_CONTEXT_PATH
+# Reading ingress host and set it as ENV
+export PUBLIC_BASE_URL=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/networking.k8s.io/v1/namespaces/entando/ingresses/quickstart-ingress | jq .spec.rules[0].host | tr -d '"')
+echo $PUBLIC_BASE_URL
 
 # Adding Context path to nginx configuration
 sed -i 's|SERVER_CONTEXT_PATH|'$SERVER_SERVLET_CONTEXT_PATH'|g' /etc/nginx/sites-available/default
-echo "CONTEXT UPDATED in NGINX"
-
+echo "CONTEX UPDATED NGINX"
 service nginx start
+pvstrapiDirPath=/entando-data/strapi-data
 
-strapiDataDirName=strapi-data
-count=0
 ####################START_SERVICES#################################################
+count=0
 while [ $count -lt 3 ]
 do
-if [ ! -f "/entando-data/$strapiDataDirName/package.json" ] && [ ${count}==0  ]
-then
-echo "====> Copying project to Persistance Volume"
-mkdir -p /entando-data/$strapiDataDirName/
-cp -r . /entando-data/$strapiDataDirName
-chown -R root /entando-data/$strapiDataDirName
-fi
+ if [ ! -f "$pvstrapiDirPath/package.json" ] && [ ${count}==0 ]
+ then
+   echo "====> copying project to Persistance Volume"
+   mkdir -p $pvstrapiDirPath
+   cp -r . $pvstrapiDirPath
+   chown -R root $pvstrapiDirPath
+ fi
 
-if [ ! -d "/entando-data/$strapiDataDirName/src/admin" ] && [ ${count}==0  ]
-then
-echo "====> Copying Custom Admin to Persistance Volume"
-cp -r ./src/admin /entando-data/$strapiDataDirName/src/
-fi
+ if grep -q "APP_KEYS" $pvstrapiDirPath/.env && grep -q "API_TOKEN_SALT" $pvstrapiDirPath/.env && grep -q "ADMIN_JWT_SECRET" $pvstrapiDirPath/.env && grep -wq "JWT_SECRET" $pvstrapiDirPath/.env
+ then
+  echo "Token Already Present"
+ else
+  sed -i.bak '$s|$|'"\nAPP_KEYS="$(openssl rand -base64 21)'|' $pvstrapiDirPath/.env
+  sed -i.bak '$s|$|'"\nAPI_TOKEN_SALT="$(openssl rand -base64 21)'|' $pvstrapiDirPath/.env
+  sed -i.bak '$s|$|'"\nADMIN_JWT_SECRET="$(openssl rand -base64 21)'|' $pvstrapiDirPath/.env
+  sed -i.bak '$s|$|'"\nJWT_SECRET="$(openssl rand -base64 21)'|' $pvstrapiDirPath/.env
+ fi
 
-if [ ! -d "/entando-data/$strapiDataDirName/node_modules" ] || [ ${count}==0 ]
-then
-  echo "===> Installing node modules"
-  npm ci --prefix /entando-data/$strapiDataDirName
-fi
+ if [ ! -d "$pvstrapiDirPath/src/admin" ] && [ ${count}==0 ]
+ then
+  echo "====> copying Custom Admin to Persistance Volume"
+  cp -r ./src/admin $pvstrapiDirPath/src/
+ fi
 
-if [ ! -d "/entando-data/$strapiDataDirName/build" ] || [ ${count}==0 ]
-then
-echo "====> configuring custom admin pannel & Building strapi"
-mv /entando-data/$strapiDataDirName/src/admin /entando-data/$strapiDataDirName/src/adminl
-npm run build --prefix /entando-data/$strapiDataDirName
-sed -i 's~@strapi/admin/strapi-server~../../../../../../src/admin/strapi-server~g' /entando-data/$strapiDataDirName/node_modules/@strapi/strapi/lib/core/loaders/admin.js
-mv /entando-data/$strapiDataDirName/src/adminl /entando-data/$strapiDataDirName/src/admin
-npm install --prefix /entando-data/$strapiDataDirName/src/admin
-fi
+ if [ ! -d "$pvstrapiDirPath/node_modules" ] || [ ${count}==0 ]
+ then
+  echo "===> installing node modules"
+  npm ci --prefix $pvstrapiDirPath
+ fi
 
-npm run develop --prefix /entando-data/$strapiDataDirName
-count=$((count+1))
-echo "Try => "$count
+ if [ ! -d "$pvstrapiDirPath/build" ] || [ ${count}==0 ]
+ then
+  echo "====> configuring custom admin pannel & Building strapi"
+  mv $pvstrapiDirPath/src/admin $pvstrapiDirPath/src/adminl
+  npm run build --prefix $pvstrapiDirPath
+  sed -i 's~@strapi/admin/strapi-server~../../../../../../src/admin/strapi-server~g' $pvstrapiDirPath/node_modules/@strapi/strapi/lib/core/loaders/admin.js
+  mv $pvstrapiDirPath/src/adminl $pvstrapiDirPath/src/admin
+  npm install --prefix $pvstrapiDirPath/src/admin
+ fi
+
+ npm run develop --prefix $pvstrapiDirPath
+ count=$((count+1))
+ echo "Try => "$count
 done
